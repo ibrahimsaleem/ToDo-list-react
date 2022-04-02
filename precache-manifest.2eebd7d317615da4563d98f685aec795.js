2d4b9eeb598815d0745ddbb667db2609721c903a
Umar <umar@Umars-MacBook-Air.local>
2022-04-03 03:30:52 +0530

    Updates

---
+++ precache-manifest.2eebd7d317615da4563d98f685aec795.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3face19f58cee41858e0",
+    "url": "/todo-list/static/css/main.7ec94302.chunk.css"
+  },
+  {
+    "revision": "3face19f58cee41858e0",
+    "url": "/todo-list/static/js/main.3face19f.chunk.js"
+  },
+  {
+    "revision": "836e4380fe3f7816a983",
+    "url": "/todo-list/static/js/1.836e4380.chunk.js"
+  },
+  {
+    "revision": "0a5029de00c51cfafc47",
+    "url": "/todo-list/static/css/2.a4eeee8d.chunk.css"
+  },
+  {
+    "revision": "0a5029de00c51cfafc47",
+    "url": "/todo-list/static/js/2.0a5029de.chunk.js"
+  },
+  {
+    "revision": "cbf2f3ec45391bce2653",
+    "url": "/todo-list/static/js/runtime~main.cbf2f3ec.js"
+  },
+  {
+    "revision": "d2097f038bf219a482724e790f8765f1",
+    "url": "/todo-list/index.html"
+  }
+];
